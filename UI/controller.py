5b7757814b7e56997a893d5a1570d9f7c288a0dd
utente <37alerusso@gmail.com>
2026-05-20 14:28:55 +0200

    Lezione 20/05

--- UI/controller.py
+++ UI/controller.py
@@ -9,14 +9,25 @@ class Controller:
         self._model = model
 
     def handleCreaGrafo(self, e):
-        self._model.creaGrafo()
+        self._model.creaGrafo(self._view._ddAnno.value)
         n, m = self._model.getGraphDetails()
         self._view._txt_result.controls.clear()
         self._view._txt_result.controls.append(ft.Text(f"Grafo correttamente creato! Il grafo è costituito da {n} nodi e {m} archi" ))
         self._view.update_page()
 
     def handleDettagli(self, e):
-        pass
+        if self._choiceTeam is None:
+            self._view._txt_result.controls.clear()
+            self._view._txt_result.controls.append(ft.Text("Selezionare un team dal menu.", color="red"))
+            self._view.update_page()
+            return
+
+        viciniTuple = self._model.getViciniTuple(self._choiceTeam)
+        self._view._txt_result.controls.clear()
+        self._view._txt_result.controls.append(ft.Text(f"Il nodo {self._choiceTeam} ha {len(viciniTuple)} vicini."))
+        self._view._txt_result.controls.append(ft.Text(f"Di seguito una lista ordinata dei vicini."), color = "green")
+        for v in viciniTuple:
+            self._view._txt_result.controls.append(ft.Text(f"{v[0]} - peso: {v[1]}"), color = "green")
 
     def handlePercorso(self, e):
         pass
